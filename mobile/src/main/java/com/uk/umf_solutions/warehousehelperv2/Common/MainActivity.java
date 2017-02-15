6a57f5c91a05acd7b00e2d3f5cb969a127bc0d8e
Eaun Ballinger <p2pjack@gmail.com>
2017-02-15 21:34:05 +0000

    Start to add view pager , tabs

--- mobile/src/main/java/com/uk/umf_solutions/warehousehelperv2/Common/MainActivity.java
+++ mobile/src/main/java/com/uk/umf_solutions/warehousehelperv2/Common/MainActivity.java
@@ -5,16 +5,66 @@ package com.uk.umf_solutions.warehousehelperv2.Common;
  * Main File
  */
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.uk.umf_solutions.warehousehelperv2.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tabs) TabLayout tabLayout;
+    @BindView(R.id.viewpager) ViewPager viewPager;
+
+
+    private Activity mActivity;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
--- mobile/src/main/java/com/uk/umf_solutions/warehousehelperv2/Ui/Wms/WmsListFragment.java
+++ mobile/src/main/java/com/uk/umf_solutions/warehousehelperv2/Ui/Wms/WmsListFragment.java
@@ -2,19 +2,36 @@ package com.uk.umf_solutions.warehousehelperv2.Ui.Wms;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.uk.umf_solutions.warehousehelperv2.Model.Wms;
 import com.uk.umf_solutions.warehousehelperv2.R;
+import com.uk.umf_solutions.warehousehelperv2.core.listeners.OnWmsSelectListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
+ *
  */
-public class WmsListFragment extends Fragment {
+public class WmsListFragment extends Fragment implements OnWmsSelectListener{
 
     private View mRootView;
+    private WmsListAdapter mAdapter;
+    @BindView(R.id.wms_list_recyclerview) RecyclerView mRecyclerView;
+    @BindView(R.id.empty_text) TextView mEmptyText;
+    @BindView(R.id.fab_wms) FloatingActionButton mFab;
 
     public WmsListFragment() {
         // Required empty public constructor
@@ -25,8 +42,41 @@ public class WmsListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mRootView = inflater.inflate(R.layout.fragment_wms_list, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_wms_list, container, true);
+        ButterKnife.bind(this,mRootView);
+
+        List<Wms> tempWms = new ArrayList<>();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new WmsListAdapter(tempWms,getContext(),this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        if (tempWms.size() < 1){
+            ShowEmptyText();
+        }else{
+            EmptyTextShowGone();
+        }
+
         return mRootView;
     }
 
+    private void EmptyTextShowGone() {
+        mEmptyText.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void ShowEmptyText() {
+
+        mRecyclerView.setVisibility(View.GONE);
+        mEmptyText.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void OnSelectedWMS(Wms selectedWMS) {
+
+    }
+
+    @Override
+    public void OnLongClickWMS(Wms clickedWMS) {
+
+    }
 }
